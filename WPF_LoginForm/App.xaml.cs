16c21de6ca76b7237fe3842798890a4f7fc52207
Юлия Огородничук <ogorodnicukulia@gmail.com>
2023-06-05 00:36:04 +0300

    First commit

---
+++ WPF_LoginForm/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF_LoginForm.View;
+using LoginView = WPF_LoginForm.View.LoginView;
+
+namespace WPF_LoginForm
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            var loginView = new LoginView();
+            loginView.Show();
+            loginView.IsVisibleChanged += (s, ev) =>
+            {
+                if (loginView.IsVisible == false && loginView.IsLoaded)
+                {
+                    var mainView = new MainView();
+                    mainView.Show();
+                    loginView.Close();
+                }
+            };
+        }
+    }
+}
---
+++ WPF_LoginForm/Repositories/RepositoryBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Windows.Markup;
+
+namespace WPF_LoginForm.Repositories
+{
+    public abstract class RepositoryBase
+    {
+        private readonly string _connectionString;
+
+        public RepositoryBase()
+        {
+            //_connectionString = @"Data Source=JULIAOHORODNICH\SQLEXPRESS;
+            //                            Initial Catalog=MVVMLoginDb;
+            //                            Integrated Security=True;
+            //                            Connect Timeout=2;
+            //                            Encrypt=False;
+            //                            TrustServerCertificate=False;
+            //                            ApplicationIntent=ReadWrite;
+            //                            MultiSubnetFailover=False";
+            _connectionString = "data source=JULIAOHORODNICH\\SQLEXPRESS;" +
+                "initial catalog=MVVMLoginDb;trusted_connection=true";
+        }
+
+        protected SqlConnection GetConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
